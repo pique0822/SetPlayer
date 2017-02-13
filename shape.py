33da29e813b0141ad7a1d722d5fd6a5ff52f532b
Miguel Del Rio <drmiguel@mit.edu>
2017-02-13 17:22:52 -0500

    completed the shape finder

--- shape.py
+++ shape.py
@@ -1,12 +1,14 @@
 import cv2
 import numpy as np
-from CardFinder import CardFinder
+import CardFinder
+
+error = 2
 
 def invert(imagem):
     imagem = (255-imagem)
     return imagem
 #need the image of the card face
-def color_of_image(img):
+def shape_of_image(img):
 
     lower = np.array([120, 120, 120])
     upper = np.array([255, 255, 255])
@@ -21,28 +23,39 @@ def color_of_image(img):
     cv2.waitKey(0)
     """
 
+    contours = []
     for c in cnts:
 
-        drawing = shapeMask.copy()
+        """drawing = shapeMask.copy()
         cv2.drawContours(drawing,[c],0,(255,255,255),2)
         cv2.imshow("image",drawing)
-        cv2.waitKey(0)
+        cv2.waitKey(0)"""
 
-        epsilon = 0.01*cv2.arcLength(c,True)
+        epsilon = 0.005*cv2.arcLength(c,True)
         approx = cv2.approxPolyDP(c,epsilon,True)
 
-        print len(approx)
-        if len(approx) == 4:
-            print "Diamond"
-        elif len(approx) == 2:
-            print "Squiggle"
-        elif len(approx) == 1:
-            print "Oval"
-        else:
-            print "Unidentified Shape"
+        contours.append(len(approx))
+
+    contours = sorted(contours)
+
+    actual_contours = []
+    for i in range(len(contours)-1):
+        if not abs(contours[i]-contours[i+1]) >= error:
+            actual_contours.append(contours[i])
+    actual_contours.append(contours[len(contours)-1])
+    contour_avg = sum(actual_contours)/len(actual_contours)
+
+    if contour_avg == 4:
+        return "Diamond"
+    elif 4 < contour_avg < 15:
+        return "Oval"
+    elif 15 <= contour_avg <= 22:
+        return "Squiggle"
+    else:
+        return "Unidentified Shape"
 
 
-finder = CardFinder()
+
 """ Works great for these images just make sure to change numCards in CardFinder
 diam = cv2.imread('imgs/green_diamond.jpg')
 print color_of_image(diam)
@@ -53,10 +66,8 @@ print color_of_image(sshape)
 oval = finder.find_cards(cv2.imread('imgs/bbg_purple_oval.jpg'))[0]
 print color_of_image(oval)
 """
-images = finder.find_cards(cv2.imread('imgs/full_set_bbg2.jpg'))
-print len(images)
+images = CardFinder.find_cards(cv2.imread('imgs/full_set.jpg'))
 for img in images:
-    print color_of_image(img)
+    print shape_of_image(img)
     cv2.imshow("image",img)
     cv2.waitKey(0)
-    print ""
