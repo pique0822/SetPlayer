2fe3e059b3885ccaa98ceb85528ac4c7bbfd81c2
Miguel Del Rio <drmiguel@mit.edu>
2017-02-13 16:55:44 -0500

    adding some functionality on shape and images

--- CardFinder.py
+++ CardFinder.py
@@ -12,13 +12,13 @@ class CardFinder:
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray,(1,1),1000)
         flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
-        
+
         #Next we look at the actual shape of the objects in the cards.
         contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key=cv2.contourArea,reverse=True)[:numcards]
-        
+
         ret = []
-        
+
         #Next we try to add a rectangle around the cards to recognize the cards
         for i in range(numcards):
             card = contours[i]
@@ -36,7 +36,7 @@ class CardFinder:
             h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
             transform = cv2.getPerspectiveTransform(dl,h)
             warp = cv2.warpPerspective(img,transform,(450,450))
-            
+
             ret.append(warp)
         return ret
 
