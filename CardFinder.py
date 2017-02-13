47e00e7709d2432064be8f3aa7baacc1caecb067
squilter <squilter@mit.edu>
2017-02-13 15:48:55 -0500

    add CardFinder.py

---
+++ CardFinder.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+
+class CardFinder:
+    def find_cards(self, img):
+        numcards = 9
+
+        #This at first removes all the unimportant details to focus on the cards.
+        #full_set_bbg2 is the better image with the whole set
+        img = cv2.resize(img, (0,0), fx=0.25, fy=0.25)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray,(1,1),1000)
+        flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
+        
+        #Next we look at the actual shape of the objects in the cards.
+        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=cv2.contourArea,reverse=True)[:numcards]
+        
+        ret = []
+        
+        #Next we try to add a rectangle around the cards to recognize the cards
+        for i in range(numcards):
+            card = contours[i]
+            peri = cv2.arcLength(card,True)
+            approx = cv2.approxPolyDP(card,0.02*peri,True)
+            rect = cv2.minAreaRect(contours[i])
+            r = cv2.cv.BoxPoints(rect)
+
+            ap = []
+            for l in approx[0:4]:
+                ap.append(l[0])
+            dl = np.array(ap,np.float32)
+
+            #This will ensure that any card that is not perfectly rectangular will be made rectangular
+            h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
+            transform = cv2.getPerspectiveTransform(dl,h)
+            warp = cv2.warpPerspective(img,transform,(450,450))
+            
+            ret.append(warp)
+        return ret
+
+'''
+cf = CardFinder()
+img = cv2.imread('imgs/full_set_bbg2.jpg')
+for im in cf.find_cards(img):
+    cv2.imshow('image',im)
+    cv2.waitKey(0)
+'''
